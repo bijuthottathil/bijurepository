de10610b31ebc42553b3d5a8a870bcdc61f4272a
bijuthottathil <63364506+bijuthottathil@users.noreply.github.com>
2022-01-01 19:43:06 -0600

    Update Firstec2.tf

--- Firstec2.tf
+++ Firstec2.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  region     = "us-west-2"
-  access_key = "AKIAYR54J6UN7ADIWNVL"
-  secret_key = "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"
-}
-
 resource "aws_instance" "myec2" {
    ami = "ami-082b5a644766e0e6f"
    instance_type = "t2.micro"
