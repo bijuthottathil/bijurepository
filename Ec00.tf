058685ba123bcbe43087ea8cbb5ef5d88f966dc8
bijuthottathil <63364506+bijuthottathil@users.noreply.github.com>
2022-01-15 18:44:10 -0600

    Update Ec00.tf

--- Ec00.tf
+++ Ec00.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIAYR54J6UN7ADIWNVL"
-  secret_key = "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"
+   access_key = "AKIAYR54J6UNZBD6UAVD"
+   secret_key = "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"
 }
 resource "aws_instance" "myec2" {
-   ami = "ami-082b5a644766e0e6f"
+   "ami-082b5a644766e0e6f"
    instance_type = "t2.micro"
 } 
